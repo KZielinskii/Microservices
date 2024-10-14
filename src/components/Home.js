4b0499b276556ad461c7ca3955d2064913cda596
KZielinskii <58587948+KZielinskii@users.noreply.github.com>
2024-10-14 15:24:19 +0200

    Improved Home view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import Tile from './Tile';
 import './Home.css';
+import game1Image from '../assets/images/ship.jpg'
+import game2Image from '../assets/images/TicTacToe.jpg';
 
 function Home() {
+    const handlePlayGame1 = () => {
+        alert('Zagraj w grę 1');
+    };
+
+    const handlePlayGame2 = () => {
+        alert('Zagraj w grę 2');
+    };
+
     return (
         <div className="home-container">
-            <div className="tile">Statki</div>
-            <div className="tile">Kółko i krzyżyk</div>
+            <Tile
+                title="Statki"
+                imageUrl={game1Image}
+                onPlay={handlePlayGame1}
+            />
+            <Tile
+                title="Kółko i Krzyżyk"
+                imageUrl={game2Image}
+                onPlay={handlePlayGame2}
+            />
         </div>
     );
 }
